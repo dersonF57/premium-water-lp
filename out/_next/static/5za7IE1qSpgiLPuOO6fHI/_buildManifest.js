181c73f0bfb9188ad6c46b81c7120b9b6e9373ee
Anderson Dev <fernandesanderson268@gmail.com>
2025-12-15 20:54:17 -0300

    Add static build output (out/) for GitHub Pages

---
+++ out/_next/static/5za7IE1qSpgiLPuOO6fHI/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/premium-water-lp//_next/:path+",
+        "destination": "/premium-water-lp/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
